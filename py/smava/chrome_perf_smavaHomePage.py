48296a2b6fd1882d8d9d90deb0eb6f7d9fe8c375
vingleshwar <vikram.ingleshwar@smava.de>
2017-12-18 11:15:01 +0100

    added info per https://github.com/godaddy/timings/issues/7

--- py/smava/chrome_perf_smavaHomePage.py
+++ py/smava/chrome_perf_smavaHomePage.py
@@ -24,6 +24,8 @@ except:
 # Setup custom config for PERF
 CONFIG_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)),'', 'config_smavaHomePage.yaml')
 PERF = Perf(CONFIG_FILE)
+
+# Refer to [POST] /v2/api/cicd/navtiming
 INJECT_CODE = PERF.injectjs('navtiming')
 
 API_PARAMS = PERF.getapiparams(es_create=True, log={'browser': BROWSER.name, 'env_tester': platform.system()})
@@ -32,6 +34,8 @@ if INJECT_CODE is not False:
     TIMING   =  BROWSER.execute_script(INJECT_CODE)
     NAV_RESP =  PERF.navtiming(TIMING, API_PARAMS)
 
+
+    # Refer to Example Response (navtiming)
     if NAV_RESP is not False:
         print(
             'PERFORMANCE was',
