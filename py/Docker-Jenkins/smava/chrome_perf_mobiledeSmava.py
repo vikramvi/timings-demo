9c5abf4f5c4657dc419ce8a1df3f1804c4e8fd4d
vingleshwar <vikram.ingleshwar@smava.de>
2018-01-31 18:42:37 +0100

    mobilede and smava integration link

---
+++ py/Docker-Jenkins/smava/chrome_perf_mobiledeSmava.py
@@ -0,0 +1,53 @@
+import os
+import platform
+from selenium import webdriver
+from timingsclient import Perf
+
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver import ChromeOptions
+
+#Jenkins Chrome Browser Fix
+#from pyvirtualdisplay import Display
+#display = Display(visible=0, size=(800, 800))  
+#display.start()
+#BROWSER = webdriver.Chrome()
+
+#BROWSER = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+chromedriver = '/usr/local/bin/chromedriver'
+os.environ["webdriver.chrome.driver"] = chromedriver
+BROWSER = RemoteWebDriver(
+    command_executor='http://0.0.0.0:4444/wd/hub',
+    desired_capabilities=DesiredCapabilities.CHROME)
+
+urlValue = "https://angebot.smava.de/?placementId=1847502766&ref=9875f8f0&leadId=07c9c5c6-1463-40db-9932-d2c269657593&adId=212073853"
+BROWSER.get(urlValue)
+
+
+try:
+    BROWSER.find_element_by_xpath('//a/img[@alt="Smava Logo"]')
+    print("FUNCTIONAL: Page looks good!")
+except:
+    print("FUNCTIONAL: Page sucks!")
+
+# Setup custom config for PERF
+CONFIG_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)),'', 'config_mobiledeSmava.yaml')
+PERF = Perf(CONFIG_FILE)
+INJECT_CODE = PERF.injectjs('navtiming')
+
+API_PARAMS = PERF.getapiparams(es_create=True, log={'browser': BROWSER.name, 'env_tester': platform.system()})
+
+if INJECT_CODE is not False:
+    TIMING   =  BROWSER.execute_script(INJECT_CODE)
+    NAV_RESP =  PERF.navtiming(TIMING, API_PARAMS)
+
+    if NAV_RESP is not False:
+        print(
+            'PERFORMANCE was',
+            ('GOOD' if NAV_RESP['assert'] is True else "BAD") + '! - ',
+              str(NAV_RESP['export']['perf']['measured']),
+              '/ ' + str(NAV_RESP['export']['perf']['threshold'])
+        )
+
+BROWSER.close()
